5680159117201fa5283b5e69b2815b4280accab7
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-03-25 08:16:06 -0400

    Removed unneeded imports

--- src/main/java/net/myteria/utils/WorldUtils.java
+++ src/main/java/net/myteria/utils/WorldUtils.java
@@ -1,57 +1,9 @@
 package net.myteria.utils;
 
 import java.io.IOException;
-import java.util.Locale;
-
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.WorldCreator;
-import org.bukkit.craftbukkit.v1_20_R3.generator.CraftWorldInfo;
-import org.bukkit.event.world.WorldLoadEvent;
-import org.bukkit.generator.BiomeProvider;
-import org.bukkit.generator.ChunkGenerator;
-
-import com.google.common.collect.ImmutableList;
-import com.mojang.serialization.Dynamic;
-import com.mojang.serialization.Lifecycle;
-
-import net.minecraft.core.IRegistry;
-import net.minecraft.core.IRegistryCustom.Dimension;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.nbt.NbtException;
-import net.minecraft.resources.MinecraftKey;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.WorldLoader;
-import net.minecraft.server.dedicated.DedicatedServer;
-import net.minecraft.server.dedicated.DedicatedServerProperties;
-import net.minecraft.server.level.ChunkProviderServer;
-import net.minecraft.server.level.WorldServer;
-import net.minecraft.util.ChatDeserializer;
-import net.minecraft.world.EnumDifficulty;
-import net.minecraft.world.entity.ai.village.VillageSiege;
-import net.minecraft.world.entity.npc.MobSpawnerCat;
-import net.minecraft.world.entity.npc.MobSpawnerTrader;
-import net.minecraft.world.level.ChunkCoordIntPair;
-import net.minecraft.world.level.EnumGamemode;
-import net.minecraft.world.level.GameRules;
-import net.minecraft.world.level.MobSpawner;
-import net.minecraft.world.level.World;
-import net.minecraft.world.level.WorldSettings;
-import net.minecraft.world.level.biome.BiomeManager;
-import net.minecraft.world.level.dimension.WorldDimension;
-import net.minecraft.world.level.levelgen.MobSpawnerPatrol;
-import net.minecraft.world.level.levelgen.MobSpawnerPhantom;
-import net.minecraft.world.level.levelgen.WorldDimensions;
-import net.minecraft.world.level.levelgen.WorldOptions;
-import net.minecraft.world.level.storage.Convertable;
-import net.minecraft.world.level.storage.LevelDataAndDimensions;
-import net.minecraft.world.level.storage.WorldDataServer;
-import net.minecraft.world.level.storage.WorldInfo;
 import net.minecraft.world.level.validation.ContentValidationException;
-import net.myteria.HousingAPI;
-import net.myteria.PlayerHousing;
+
 
 public class WorldUtils {
 	public org.bukkit.World createWorld(WorldCreator creator) throws IOException, ContentValidationException {
